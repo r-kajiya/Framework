4986948a64e806a0b1edbef00c58f16fde320aed
r-kajiya <ryuseikajiya@ryuseinoMacBook-Pro.local>
2020-01-11 20:07:34 +0900

    MonoSingleton追加

---
+++ Script/Singleton/MonoSingleton.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Framework
+{
+    public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+
+        public static T I
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+
+                    if (_instance == null)
+                    {
+                        GameObject go = new GameObject();
+                        go.name = typeof(T).Name;
+                        _instance = go.AddComponent<T>();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        protected void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
+                OnAwake();
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        protected virtual void OnAwake(){}
+    }	
+}
